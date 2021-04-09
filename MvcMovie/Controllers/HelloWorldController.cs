bd875c4c42a88a0974ed575551dfab651e815607
reda-coder <redaelouahabi98@gmail.com>
2021-04-09 19:07:26 +0100

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //GET:/helloworld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+          
+        //GET:/helloworld/welcome/
+        // public string welcome(string name,int ID = 1)
+        public IActionResult welcome(string name, int numtimes = 1)
+        {
+
+            //return System.Text.Encodings.Web.HtmlEncoder.Default.Encode($"Salut {name}, Numtimes est : {ID}");
+            ViewData["message"] = "Salut" + name;
+            ViewData["numTimes"] = numtimes;
+            return View();
+         
+        }
+    }
+}
---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    
+    
+    public static  class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<DbContextOptions<MvcMovieContext>>()))
+            {
+                //look for any movies.
+                if (context.Movie.Any())
+                {
+                    return; //db has been seeded
+                }
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When harry mer Sally",
+                        ReleaseDate = DateTime.Parse("1998-2-12"),
+                        Genre = "Romantic Comedy",
+                        Rating = "R",
+                        Price = 7.99M
+                        
+
+                    },
+                      new Movie
+                      {
+                          Title = "GhostBusters",
+                          ReleaseDate = DateTime.Parse("1998-3-13"),
+                          Genre = " Comedy",
+                          Rating = "R",
+
+                          Price = 8.99M
+
+                      },
+                      new Movie
+                      {
+                          Title = "GhostBusters 2",
+                          ReleaseDate = DateTime.Parse("1999-9-3"),
+                          Genre = " Comedy",
+                          Rating = "R",
+                          Price =98.99M
+
+                      },
+                       new Movie
+                       {
+                           Title = "Rio Bravo",
+                           ReleaseDate = DateTime.Parse("1958-7-16"),
+                           Genre = "Western",
+                           Rating = "R",
+                           Price = 3.99M
+
+                       }
+                    );
+                context.SaveChanges();
+            }
+        }
+
+
+    }
+}
+             
